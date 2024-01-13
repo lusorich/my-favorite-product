cca13ec238f3a7145fde22552372d09248694d92
dreamonaut <dreamonautre@gmail.com>
2024-01-13 22:41:43 +0500

    T-0002: add svg and Icon component

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,7 +1,12 @@
+import { ICON_NAME } from "shared/utils/constants";
+import { Icon } from "../Icon/Icon";
+
 export const Header = () => {
   return (
     <header>
-      <div>Logo</div>
+      <div>
+        <Icon name={ICON_NAME.ROBOT} />
+      </div>
       <div>signin</div>
     </header>
   );
